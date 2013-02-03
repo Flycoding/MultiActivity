57b5286bffea19a88d28f1806b5487bbca177e89
flycoding <flycoding@yeah.net>
2013-02-03 20:50:55 +0800

    test multiple activity.

---
+++ gen/com/flyingh/multiactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.flyingh.multiactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/flyingh/multiactivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.flyingh.multiactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void startAnother(View view) {
+		startActivity(new Intent(getApplicationContext(), AnotherActivity.class));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
